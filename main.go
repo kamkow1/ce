eb8bd0c3f39f572810be4dddf9852e63ec04a246
kamkow1 <ikamileq@gmail.com>
2023-02-25 19:51:23 +0100

    Draw empty screen with tcell

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import "log"
+
+import "github.com/gdamore/tcell"
 
 func main() {
-  fmt.Println("hello world")
+  defaultStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorBlack)
+  screen, err := tcell.NewScreen()
+  if err != nil {
+    log.Fatalf("%+v", err)
+  }
+  if err := screen.Init(); err != nil {
+    log.Fatalf("%+v", err)
+  }
+  screen.SetStyle(defaultStyle)
+  screen.EnableMouse()
+  screen.Clear()
+
+  quit := func() {
+    maybePanic := recover()
+    screen.Fini()
+    if maybePanic != nil {
+      panic(maybePanic)
+    }
+  }
+  defer quit()
+
+  for {
+    screen.Show()
+    event := screen.PollEvent()
+    switch event := event.(type) {
+    case *tcell.EventResize:
+      screen.Sync()
+    case *tcell.EventKey:
+      key := event.Key()
+      if key == tcell.KeyEscape || key == tcell.KeyCtrlC {
+        return
+      }
+    }
+  }
 }
